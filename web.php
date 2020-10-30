8970301b1905555747c1ef60a88f05d5f97b6786
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-10-30 12:54:08 +0700

    Update web.php

--- web.php
+++ web.php
@@ -1,20 +1,20 @@
 <html>
     <head>
-        <title>Hello world</title>
+        <meta charset="UTF-8">
+        <title>New !!!</title>
     </head>
     <body>
-        <h1>My First Heading</h1>
+        <h1>Woranun Wareeprasert (Tax)</h1>
         <p>My First Paragraph</p>
         <img src="cat.jpg">
+        <img src="cat1.jpg">
         <br>
-        <a href="http://www.it.kmitl.ac.th">This is a link</a>
-
-        <h2>The unordered list</h2>
+        <a href="https://www.reg.kmitl.ac.th/index/">This is a link</a>
+        <h2>เรื่องฮาๆ ตอนขับรถ ก็ตอนแฟนตดในรถนี่แหละ ขับอยู่ดีๆ ได้กลิ่นโชยแบบรุนแรงมาก กลิ่นได้ที่ขมคอกันเลยทีเดียว เปิดกระจกรถแทบไม่ทัน ทั้งขำทั้งโมโห จะตดไม่บอกไม่กล่าวล่วงหน้า ไม่งั้นจะไล่ลงไป กลิ่นตลบอบอวลมึนหัวไปทั้งคัน แต่แฟนบอกว่าเคยอ่านนิตยสารเล่มหนึ่ง เขียนไว้ว่า การดมตดดีต่อสุขภาพ แถมยังกระชับความสัมพันธ์คู่รักได้ดี เลยต้องให้อภัย คิดซะว่าแฟนรักแฟนหลง แค่อยากให้เราแข็งแรง</h2>
         <ul>
             <li>Google</li>
             <li>Yahoo</li>
             <li>Amazon</li>
         </ul>
-
     </body>
 </html>
