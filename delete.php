c3121fc251def1dc08c4186051c289668b1a8383
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-11-17 17:04:40 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'lab02.mysql.database.azure.com', 'woranun@lab02', 'VGTlfl45', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+
+
+$sql = "DELETE FROM guestbook WHERE Name='$name';
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
