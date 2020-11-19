23f6d7227ca43df52e8ff5157756d8db8e82a16f
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-11-19 14:11:15 +0700

    Update show1.php

--- show1.php
+++ show1.php
@@ -19,7 +19,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 
 <div class="container">
-  <table width="800" border="1" class="#F5B7B1" class="center" >
+  <table width="800" border="1" class="RGB(255, 160, 122)" class="center" >
     <thead class="thead-dark">
     <tr class="active">
       <th width="200"> <div align="center" class="alert alert-success">Name</div></th>
