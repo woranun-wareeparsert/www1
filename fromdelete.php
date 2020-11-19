51830b82028cfd121276207db8721c91016fbd0a
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-11-19 12:42:18 +0700

    Add files via upload

---
+++ fromdelete.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Delete Form</title>
+<meta charset="utf-8">
+</head>
+<body>
+  <form action = "delete.php" method = "post" id="CommentForm" >
+    Name want to delete:<br>
+    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
+    <input type="submit" id="commentBtn">
+  </form> 
+</body>
+</html>
