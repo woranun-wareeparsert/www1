04cf8b7f0c232395bdb71cf08a984eccd6bf1a34
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-11-20 03:15:21 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="generator" content="Google Web Designer 9.0.2.0915">
+  <meta name="template" content="Banner 3.0.0">
+  <meta name="environment" content="gwd-genericad">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="gwdpage_style.css" rel="stylesheet" data-version="13" data-exports-type="gwd-page">
+  <link href="gwdpagedeck_style.css" rel="stylesheet" data-version="14" data-exports-type="gwd-pagedeck">
+  <link href="gwdimage_style.css" rel="stylesheet" data-version="15" data-exports-type="gwd-image">
+  <style type="text/css" id="gwd-lightbox-style">
+    .gwd-lightbox {
+      overflow: hidden;
+    }
+  </style>
+  <style type="text/css" id="gwd-text-style">
+    p {
+      margin: 0px;
+    }
+    h1 {
+      margin: 0px;
+    }
+    h2 {
+      margin: 0px;
+    }
+    h3 {
+      margin: 0px;
+    }
+  </style>
+  <style type="text/css">
+    html, body {
+      width: 100%;
+      height: 100%;
+      margin: 0px;
+    }
+    .gwd-page-container {
+      position: relative;
+      width: 100%;
+      height: 100%;
+    }
+    .gwd-page-content {
+      background-color: transparent;
+      transform: perspective(1400px) matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+      transform-style: preserve-3d;
+      position: absolute;
+    }
+    .gwd-page-wrapper {
+      background-color: rgb(255, 255, 255);
+      position: absolute;
+      transform: translateZ(0px);
+    }
+    .gwd-page-size {
+      width: 1980px;
+      height: 720px;
+    }
+    .gwd-image-1xjd {
+      position: absolute;
+      left: 0px;
+      transform-origin: 50% 50% 0px;
+      width: 1980px;
+      top: 0px;
+      height: 720px;
+    }
+    .gwd-h2-1mk7 {
+      position: absolute;
+      font-family: Pangolin;
+      font-style: italic;
+      font-size: 50px;
+      color: rgb(167, 32, 88);
+      left: 502px;
+      top: 200px;
+      transform-origin: 402px 48px 0px;
+    }
+  </style>
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa:300,regular,500,600,700|Righteous:regular|Montserrat+Alternates:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic|Space+Mono:regular,italic,700,700italic|Amaranth:regular,italic,700,700italic|Sawarabi+Gothic:regular|Pangolin:regular" rel="stylesheet" type="text/css">
+  <script data-source="googbase_min.js" data-version="4" data-exports-type="googbase" src="googbase_min.js"></script>
+  <script data-source="gwd_webcomponents_v1_min.js" data-version="2" data-exports-type="gwd_webcomponents_v1" src="gwd_webcomponents_v1_min.js"></script>
+  <script data-source="gwdpage_min.js" data-version="13" data-exports-type="gwd-page" src="gwdpage_min.js"></script>
+  <script data-source="gwdpagedeck_min.js" data-version="14" data-exports-type="gwd-pagedeck" src="gwdpagedeck_min.js"></script>
+  <script data-source="gwdgenericad_min.js" data-version="6" data-exports-type="gwd-genericad" src="gwdgenericad_min.js"></script>
+  <script data-source="gwdimage_min.js" data-version="15" data-exports-type="gwd-image" src="gwdimage_min.js"></script>
+</head>
+
+<body>
+  <gwd-genericad id="gwd-ad">
+    <gwd-pagedeck class="gwd-page-container" id="pagedeck">
+      <gwd-page id="page1" class="gwd-page-wrapper gwd-page-size gwd-lightbox" data-gwd-width="1980px" data-gwd-height="720px">
+        <div class="gwd-page-content gwd-page-size">
+          <gwd-image id="坂内拓_on_Twitter" source="assets/坂内拓 on Twitter.jpg" scaling="stretch" class="gwd-image-1xjd"></gwd-image>
+          <h1 class="gwd-h2-1mk7">HOW'S YOUR DAY GOING ?</h1></div>
+      </gwd-page>
+    </gwd-pagedeck>
+  </gwd-genericad>
+  <script type="text/javascript" id="gwd-init-code">
+    (function() {
+      var gwdAd = document.getElementById('gwd-ad');
+
+      /**
+       * Handles the DOMContentLoaded event. The DOMContentLoaded event is
+       * fired when the document has been completely loaded and parsed.
+       */
+      function handleDomContentLoaded(event) {
+
+      }
+
+      /**
+       * Handles the WebComponentsReady event. This event is fired when all
+       * custom elements have been registered and upgraded.
+       */
+      function handleWebComponentsReady(event) {
+        // Start the Ad lifecycle.
+        setTimeout(function() {
+          gwdAd.initAd();
+        }, 0);
+      }
+
+      /**
+       * Handles the event that is dispatched after the Ad has been
+       * initialized and before the default page of the Ad is shown.
+       */
+      function handleAdInitialized(event) {}
+
+      window.addEventListener('DOMContentLoaded',
+        handleDomContentLoaded, false);
+      window.addEventListener('WebComponentsReady',
+        handleWebComponentsReady, false);
+      window.addEventListener('adinitialized',
+        handleAdInitialized, false);
+    })();
+  </script>
+</body>
+
+</html>
