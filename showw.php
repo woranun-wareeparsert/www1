6832957531e995a58d173c53b3e6f7e297317a66
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-11-19 13:12:00 +0700

    Update showw.php

--- showw.php
+++ showw.php
@@ -1,6 +1,5 @@
 <html>
 <head>
-<title>How are you today</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -20,9 +19,6 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 
 <div class="container">
-    <div class="form">
-        <p class="ex1"><h1><center>How are you today</center></h1></p>
-    </div>
   <table width="800" border="1" class="alert alert-warning" class="center" >
     <thead class="thead-dark">
     <tr class="active">
