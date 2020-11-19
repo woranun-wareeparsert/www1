2da9986333b30be158ed67bcbf67d93497baabf2
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-11-19 15:14:27 +0700

    Update formedit.php

--- formedit.php
+++ formedit.php
@@ -6,9 +6,9 @@
 </head>
 <body>
   <form action = "edit2.php" method = "post" id="CommentForm" >
-    ชื่อของคนที่จะแก้:<br>
+    Name:<br>
     <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
-    ความรู้สึกที่เปลี่ยนไป:<br>
+    Comment:<br>
     <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter your feel"></textarea><br>  
     <input type="submit" id="commentBtn">
   </form> 
