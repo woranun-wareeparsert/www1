5ea1db5d8ca89f802a2f72d83f3ba7b0ef31589b
woranun-wareeparsert <73680983+woranun-wareeparsert@users.noreply.github.com>
2020-11-19 15:20:01 +0700

    Update formedit.php

--- formedit.php
+++ formedit.php
@@ -9,7 +9,7 @@
     Name:<br>
     <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
     Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter your feel"></textarea><br>  
+    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter your comment"></textarea><br>  
     <input type="submit" id="commentBtn">
   </form> 
 </body>
